efe4239c55f0e80f3bc84558249daf962e68a9e2
Omar Al Taher <omar@altaher.net>
2016-08-08 20:15:35 -0700

    Adding loader file. Changes include converting numerical columns to numerical datatype and converting the treatment date to datetime type. Added textfind and notereader functions

---
+++ Python_Project2/loader.py
@@ -0,0 +1,139 @@
+import csv 
+import pandas as pd
+import matplotlib.pyplot as plt
+%matplotlib inline  
+
+with open('Data/2015.tsv','r') as tsv:
+    data = [line.strip().split('\t') for line in tsv]
+    
+col_names = data[0]
+df = pd.DataFrame(data, columns=col_names)
+df = df[1:]
+
+#open code_sex.csv and convert it to a DataFrame
+with open('Data/code_sex.csv','r') as csv:
+    sex_descrip = [line.strip().split(',') for line in csv]
+
+sex_header = sex_descrip[0]
+df_sex = pd.DataFrame(sex_descrip, columns=sex_header)
+df_sex = df_sex[1:]
+indexed_df_sex = df_sex.set_index('Code')
+
+#open code_body_part.csv and convert it to a DataFrame
+with open('Data/code_body_part.csv','r') as csv:
+    body_part_descrip = [line.strip().split(',') for line in csv]
+
+body_part_header = body_part_descrip[0]
+df_body_part = pd.DataFrame(body_part_descrip, columns=body_part_header)
+df_body_part = df_body_part[1:]
+indexed_df_body_part = df_body_part.set_index('Code')
+
+#open code_diagnosis.csv and convert it to a DataFrame
+with open('Data/code_diagnosis.csv','r') as csv:
+    diagnosis_descrip = [line.strip().split(',') for line in csv]
+
+diagnosis_header = diagnosis_descrip[0]
+df_diagnosis = pd.DataFrame(diagnosis_descrip, columns=diagnosis_header)
+df_diagnosis = df_diagnosis[1:]
+indexed_df_diagnosis = df_diagnosis.set_index('Code')
+
+#open code_disposition.csv and convert it to a DataFrame
+with open('Data/code_disposition.csv','r') as csv:
+    disposition_descrip = [line.strip().split(',') for line in csv]
+
+disposition_header = disposition_descrip[0]
+df_disposition = pd.DataFrame(disposition_descrip, columns=disposition_header)
+df_disposition = df_disposition[1:]
+indexed_df_disposition = df_disposition.set_index('Code')
+
+#open code_fire.csv and convert it to a DataFrame
+with open('Data/code_fire.csv','r') as csv:
+    fire_descrip = [line.strip().split(',') for line in csv]
+
+fire_header = fire_descrip[0]
+df_fire = pd.DataFrame(fire_descrip, columns=fire_header)
+df_fire = df_fire[1:]
+indexed_df_fire = df_fire.set_index('Code')
+
+#open code_locale.csv and convert it to a DataFrame
+with open('Data/code_locale.csv','r') as csv:
+    locale_descrip = [line.strip().split(',') for line in csv]
+
+locale_header = locale_descrip[0]
+df_locale = pd.DataFrame(locale_descrip, columns=locale_header)
+df_locale = df_locale[1:]
+indexed_df_locale = df_locale.set_index('Code')
+
+#open code_product.csv and convert it to a DataFrame
+with open('Data/code_product.csv','r') as csv:
+    product_descrip = [line.strip().split(',',1) for line in csv]
+
+product_header = product_descrip[0]
+df_product = pd.DataFrame(product_descrip, columns=product_header)
+df_product = df_product[1:]
+indexed_df_product = df_product.set_index('Code')
+
+#open code_race.csv and convert it to a DataFrame
+with open('Data/code_race.csv','r') as csv:
+    race_descrip = [line.strip().split(',') for line in csv]
+
+race_header = race_descrip[0]
+df_race = pd.DataFrame(race_descrip, columns=race_header)
+df_race = df_race[1:]
+indexed_df_race = df_race.set_index('Code')
+
+#merge all the codes
+df['sex_descrip']=df.sex.map(indexed_df_sex.Description)
+df['body_part_descrip']=df.body_part.map(indexed_df_body_part.Description)
+df['diag_descrip']=df.diag.map(indexed_df_diagnosis.Description)
+df['disposition_descrip']=df.disposition.map(indexed_df_disposition.Description)
+df['fire_descrip']=df.fmv.map(indexed_df_fire.Description)
+df['locale_descrip']=df.location.map(indexed_df_locale.Description)
+df['product1_descrip']=df.prod1.map(indexed_df_product.Description)
+df['product2_descrip']=df.prod2.map(indexed_df_product.Description)
+df['race_descrip']=df.race.map(indexed_df_race.Description)
+
+#combine the 2 narr fields into 1
+df['Notes'] = df['narr1'].map(str) + df['narr2'].map(str)
+
+df1 = df[['CPSC Case #', 
+         'trmt_date', 
+         'psu', 
+         'weight', 
+         'age', 
+         'sex_descrip', 
+         'race_descrip',
+         'body_part_descrip', 
+         'diag_descrip', 
+         'disposition_descrip', 
+         'fire_descrip', 
+         'locale_descrip', 
+         'product1_descrip', 
+         'product2_descrip', 
+         'Notes']]
+
+#convert numerical columns to numeric data type
+df1 = df1.apply(lambda x: pd.to_numeric(x,errors="ignore"))
+
+#convert 'trmt_date' column to datetime type
+df1['trmt_date'] = df1['trmt_date'].apply(lambda x: pd.to_datetime(x,format='%m/%d/%Y'))
+
+
+def textfind(dataframe, field, string):
+    """ Takes dataframe, field (column in the dataframe), and a string to search
+    Allow for use of regular expressions.
+    Returns a dataframe of the records where the string is found"""
+    import re
+    a = [df1.loc[i] for i in range(1,len(dataframe)) \
+         if re.search(string, dataframe[field][i])]
+    return pd.DataFrame(a)
+    
+def notereader(dataframe):
+    """ Takes a dataframe that contains the 'Notes' field and prints it out in a
+    readable fashion.
+    Returns a dictionary of Index (integer): Note (string) pairs"""
+    output_dict ={}
+    for i in dataframe.index:           
+        output_dict[i] = dataframe.Notes[i]
+        print ("INDEX", i,"\n", dfjunk.Notes[i],"\n")
+    return output_dict
